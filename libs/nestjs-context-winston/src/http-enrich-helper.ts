39da0ca13841cc62bfc95e0cb9c3c96a1e6dea57
Thiago Santos <santos.thiago@grupoboticario.com.br>
2025-10-15 05:07:13 -0300

    feat: creating privacy headers list

--- libs/nestjs-context-winston/src/http-enrich-helper.ts
+++ libs/nestjs-context-winston/src/http-enrich-helper.ts
@@ -1,5 +1,28 @@
 import { HttpRequest } from './context-logging-options';
 
+export const BASIC_HTTP_ENRICH_EXCLUSION = Object.freeze([
+	'authorization',
+	'cookie',
+	'x-api-key',
+]);
+export const HTTP_PRIVACY_HEADERS = Object.freeze([
+	'set-cookie',
+	'cookie',
+	'authorization',
+	'x-api-key',
+	'x-user-id',
+	'x-customer-id',
+	'x-session-id',
+	'x-forwarded-for',
+	'forwarded',
+	'x-device-id',
+	'x-app-id',
+	'x-email',
+	'x-phone-number',
+	'x-access-token',
+	'x-refresh-token',
+]);
+
 /**
  * Generates data from a http request to enrich log metadata based on its headers.
  * By default, it excludes the 'authorization' and 'cookie' headers to avoid logging sensitive information.
@@ -8,7 +31,7 @@ import { HttpRequest } from './context-logging-options';
  * @returns A function that takes a HttpRequest and returns an object with filtered headers.
  */
 export function httpEnrichHelper(
-	excludeFields: string[] = ['authorization', 'cookie'],
+	excludeFields: readonly string[] = HTTP_PRIVACY_HEADERS,
 ) {
 	const fieldsSet = new Set(excludeFields.map((f) => f.toLowerCase()));
 	return (req: HttpRequest) => {

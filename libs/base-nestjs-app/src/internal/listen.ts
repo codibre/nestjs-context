d0d454c5cc8fa9c181b627ba5fd82a0eccff96bc
Thiago Santos <santos.thiago@grupoboticario.com.br>
2025-12-06 12:25:07 -0300

    fix: adding port conflict dynamic solving
    If a microsservice listen to the same port of the
    main server, the port will be changed randomly and
    a warning will be logged. This is good to deal with
    sqs microsservices, that are not natively supported by
    NestJs and often are custom implemented without having
    port defined

--- libs/base-nestjs-app/src/internal/listen.ts
+++ libs/base-nestjs-app/src/internal/listen.ts
@@ -1,16 +1,56 @@
 import { INestApplication } from '@nestjs/common';
 import { BaseNestjsOptions } from '../types';
+import { Server, createServer } from 'net';
 
 const DEFAULT_PORT = 3000;
+
+async function getUnusedPortOtherThan(mainPort: number) {
+	const servers: Server[] = [];
+	let port = 0;
+	do {
+		const server = createServer();
+		await new Promise<void>((resolve, reject) => {
+			server.on('error', reject);
+			server.listen(0, () => {
+				servers.push(server);
+				const address = server.address();
+				if (address && typeof address === 'object') {
+					const serverPort = address.port;
+					if (serverPort !== mainPort) port = serverPort;
+				} else {
+					reject(new Error('Could not get server address'));
+				}
+				resolve();
+			});
+		});
+	} while (!port || port === mainPort);
+	servers.forEach((server) => server.close());
+	return port;
+}
 
 export async function listen(
 	app: INestApplication,
 	options: BaseNestjsOptions,
 ) {
+	const mainPort = options.server?.port ?? DEFAULT_PORT;
 	if (options.microservices?.length) {
+		for (const ms of app.getMicroservices()) {
+			if (
+				'serverInstance' in ms &&
+				typeof ms.serverInstance === 'object' &&
+				ms.serverInstance &&
+				'port' in ms.serverInstance &&
+				ms.serverInstance.port === mainPort
+			) {
+				ms.serverInstance.port = await getUnusedPortOtherThan(mainPort);
+				options.loggingModule.logger.warn(
+					`Microservice port conflicted with main server port. Changed microservice port to ${ms.serverInstance.port}`,
+				);
+			}
+		}
 		await app.startAllMicroservices();
 	}
-	await app.listen(options.server?.port ?? DEFAULT_PORT, '0.0.0.0');
+	await app.listen(mainPort, '0.0.0.0');
 	options.loggingModule.logger.info(`Listening on ${await app.getUrl()}`);
 	return app;
 }
